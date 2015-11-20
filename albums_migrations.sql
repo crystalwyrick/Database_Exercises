7e6f21f6875787225d132b256966449f4d67b662
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-20 09:15:53 -0600

    updated file - exercise 8.3 - created table

--- albums_migrations.sql
+++ albums_migrations.sql
@@ -1,1 +1,15 @@
-this is a file
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	release_date YEAR(4) NOT NULL,
+	sales FLOAT NOT NULL,
+	genre VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
