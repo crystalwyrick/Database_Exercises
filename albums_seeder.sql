f3f258f74042f4b583a823669923654c0418762b
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-20 11:11:34 -0600

    exercise 8.4.1 - table insert

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre) 
+VALUES 	('Michael Jackson','Thriller', '1982', '43.3', 'pop, rock, R&B'),
+		('Eagles', 'Their Greatest Hits', '1976', '32.2', 'rock, soft rock, folk rock'),
+		('AC/DC', 'Back in Black', '1980', '25.9', 'hard rock'),
+		('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'progressive rock'),
+		('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'soundtrack/R&B,soul, pop');
