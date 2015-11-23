6e411711c39406c35b116b955a7eaadc19068eff
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-23 13:35:05 -0600

    8.7.1. query Christmas birthday

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,4 +8,8 @@ WHERE last_name LIKE 'E%';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'; 
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
