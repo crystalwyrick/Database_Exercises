4e7d1beacb62a9b4a650d3b024504fb93e736ea4
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-23 14:00:43 -0600

    8.7.2. chaining WHERE clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,19 +1,35 @@
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya'); 
+WHERE first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya'; 
+	
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE gender = 'M'
+	AND (
+		first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya' 
+	);
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%'; 
+WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%E'; 
+	
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+	AND last_name LIKE '%E'; 
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+	AND birth_date LIKE '%-12-25';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE birth_date LIKE '%-12-25';
-
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+	AND last_name NOT LIKE '%qu%'
+	
