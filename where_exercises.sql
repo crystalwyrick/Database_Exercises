c488af4c93d075526dd8bf7dd78d818f45c4243b
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-23 13:32:11 -0600

    8.7.1. Sequel Pro exercises: query hire date in the 90's

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,3 +6,6 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'; 
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'; 
